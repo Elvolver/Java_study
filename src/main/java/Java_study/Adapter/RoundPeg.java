e002fc15060800b23b78570bfd11bdf46a4007d3
volkov <volkov@vologda.softlab.ru>
2019-01-21 21:52:39 +0300

    cC

---
+++ src/main/java/Java_study/Adapter/RoundPeg.java
@@ -0,0 +1,11 @@
+package Java_study.Adapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: volkov
+ * Date: 21.01.19
+ * Time: 19:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class RoundPeg {
+}
